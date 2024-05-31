e9922fe9d5ff769cb118243a7b5a9711fa3df9d6
nookoid <christopher.morrison@travelpassgroup.com>
2024-05-31 08:49:21 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const main = document.body.children[1]
+
+const heroImage = main.appendChild(document.createElement('img'))
+heroImage.setAttribute('src', './assets/boat-beach.webp')
+heroImage.setAttritube('alt', 'beach with palm trees')
