b230b9498e5e6c411b610a1aabfa88256fa55c24
nookoid <christopher.morrison@travelpassgroup.com>
2024-06-01 22:48:11 -0600

    Add new image files with updated names

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const main = document.body.children[1]
 
 const heroImage = main.appendChild(document.createElement('img'))
-heroImage.setAttribute('src', './assets/boat-beach.webp')
-heroImage.setAttritube('alt', 'beach with palm trees')
+// heroImage.setAttribute('src', './assets/boat-beach-768.png')
+heroImage.setAttribute(
+  'srcset',
+  './assets/boat-beach-768.png 768w, ./assets/boat-beach-1025.png 1024w, ./assets/boat-beach-1280.png 1280w,'
+)
